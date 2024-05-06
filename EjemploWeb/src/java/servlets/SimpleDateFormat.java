a632147bdead74e07dcb55ff3011191e84b19cae
rcr03 <rcr030515@gmail.com>
2024-05-05 22:33:32 -0600

    r

---
+++ EjemploWeb/src/java/servlets/SimpleDateFormat.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package servlets;
+
+/**
+ *
+ * @author ReginaCR
+ */
+public class SimpleDateFormat {
+
+    public SimpleDateFormat(String ddMMyyyy) {
+    }
+
+    Date parse(String strfecha) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
